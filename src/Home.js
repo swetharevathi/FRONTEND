0d111809113c34b3375b873c313dff503bb90fb8
swetharevathi <swetharevathi@gmail.com>
2024-03-11 04:04:22 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW-b2tedfc7ux5SruCgTzuG1Or6h4eyv5eIFWtJwjyfN9aKo07JBWxBTXYLoro61JmeBk&usqp=CAU"/>
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://backend-sigma-amber.vercel.app"
